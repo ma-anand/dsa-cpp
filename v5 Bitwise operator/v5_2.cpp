ff6faed7325586fff2441a50c40efa47b88b6634
ma-anand <aanand20-comp@bvucoep.edu.in>
2023-03-08 01:43:44 +0530

    Right Shift (>>),     Left Shift(<<)

---
+++ v5 Bitwise operator/v5_2.cpp
@@ -0,0 +1,10 @@
+//Right Shift (>>),     Left Shift(<<)
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    cout<<(15>>1)<<endl;          //this is right shift. Right shift = number/(2*no. of right shift)
+    
+    cout<<(3<<2)<<endl;           //this is left shift. Left shift = number*(2*no. of right shift)
+}
