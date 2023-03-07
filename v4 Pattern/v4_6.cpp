a2379e21a252cbb229762e4d396e21ad30005945
ma-anand <aanand20-comp@bvucoep.edu.in>
2023-03-08 01:35:49 +0530

    Squar by Character increasing form

---
+++ v4 Pattern/v4_6.cpp
@@ -0,0 +1,17 @@
+//Squar by Character increasing form
+#include<iostream>
+using namespace std;
+
+int main(){
+    int row;
+    cin>>row;
+    char a='A';
+    for(int i=0;i<row;i++){
+        for(int j=0;j<row;j++){
+            cout<<a<<" ";
+        }
+        a++;
+        cout<<endl;
+    }
+    return 0;
+}
