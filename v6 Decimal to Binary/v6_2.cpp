33bd977f8471ae088ac1f6b1ebe84e8e78cac4b8
ma-anand <aanand20-comp@bvucoep.edu.in>
2023-03-08 01:46:51 +0530

    Negative decimal number to binary number

---
+++ v6 Decimal to Binary/v6_2.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+
+                   //negative decimal number to binary number
+
+
+int main(){
+    int negative_decimal;       //only negative number input will give right answer.
+    cin>>negative_decimal;
+    
+    return 0;
+}
