97df536e6fa76f0ff71b4619c8bad146f406e1a8
ma-anand <aanand20-comp@bvucoep.edu.in>
2023-03-08 01:33:05 +0530

    Squar pattern by no. of column

---
+++ v4 Pattern/v4_1.cpp
@@ -0,0 +1,15 @@
+//squar pattern by no. of column
+#include<iostream>
+using namespace std;
+
+int main(){
+    int row;
+    cin>>row;
+    for(int i=0;i<row;i++){
+        for(int j=1;j<=row;j++){
+            cout<<j<<" ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
