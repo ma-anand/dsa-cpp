3e01dc0f1d0576b750052cc3310216cf7abc5628
ma-anand <aanand20-comp@bvucoep.edu.in>
2023-03-21 00:54:11 +0530

    Pair Sum

--- v8 Array/v8_9.cpp
+++ v8 Array/v8_9.cpp
@@ -1,10 +1,40 @@
-//
-//14 of March,2023
+//Pair Sum
+//21st of march, 2023
+
 #include<iostream>
+#include<vector>
+#include<math.h>
+#include <algorithm>
 using namespace std;
 
 int main(){
-    int arr[]={1,2,3,4,5};
-    cout<<"ARR[0]"<<arr[0]<<endl;
-    return 0;
+    //vector<int> arr={1,9,2,8,3};
+    //int sizeOfArr=5,sumIsEqual=10;
+    vector<int> arr;
+    vector <vector<int>> ans;
+    int sizeOfArr,sumIsEqual;
+    cin>>sizeOfArr>>sumIsEqual;
+    for(int i=0;i<sizeOfArr;i++){
+        int a;
+        cin>>a;
+        arr.push_back(a);
+    }
+    for(int i=0;i<sizeOfArr;i++){
+        for(int j=i+1;j<sizeOfArr;j++){
+            if(arr[i]+arr[j]==sumIsEqual){
+                vector<int> temp;
+                temp.push_back(min(arr[i],arr[j]));
+                temp.push_back(max(arr[i],arr[j]));
+                ans.push_back(temp);
+            }
+        }
+    }
+    sort(ans.begin(),ans.end());
+    for(int i=0;i<ans.size();i++){
+        for(int j=0;j<2;j++){
+            cout<<ans[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+    
 }
