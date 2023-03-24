3789efe697208e553a2b28d7801f57fce9fa5dd2
ma-anand <aanand20-comp@bvucoep.edu.in>
2023-03-25 01:35:18 +0530

    Multiplication of Array

--- v8 Array/v8_11.cpp
+++ v8 Array/v8_11.cpp
@@ -3,7 +3,28 @@
 #include<iostream>
 using namespace std;
 
+inputMatrix(int arr[], int rowSize){
+    for(int i=0;i<rowSize;i++){
+        for(int j=0;j<rowSize;j++){
+            cin>>arr[i][j];
+        }
+    }
+}
+
+outputMatrix(int arr[], int rowSize){
+    for(int i=0;i<rowSize;i++){
+        for(int j=0;j<rowSize;j++){
+            cout<<arr[i][j];
+        }
+        cout<<endl;
+    }
+}
+
 int main(){
-    int i;
+    int arr1[2][2],arr2[2][2];
+    inputMatrix(arr1,2,2);
+    inputMatrix(arr2,2,2);
+    outputMatrix(arr1,2,2);
+    outputMatrix(arr2,2,2);
     return 0;
 }
