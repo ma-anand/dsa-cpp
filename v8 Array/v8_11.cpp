c5952d68572136c2a5139bf9a4d9dedc84470fc4
ma-anand <aanand20-comp@bvucoep.edu.in>
2023-03-26 15:38:50 +0530

    Pair in C++ (STL)

--- v8 Array/v8_11.cpp
+++ v8 Array/v8_11.cpp
@@ -1,29 +1,27 @@
-//Multiplication of Matrix
+//Pair in C++ Standard Template Library (STL)
 //22nd of March, 2023
+//First and Last Position of an Element In Sorted Array
+//26th of March, 2023
 #include<iostream>
+#include <utility>
 using namespace std;
 
-inputMatrix(int arr[], int rowSize){
-    for(int i=0;i<rowSize;i++){
-        for(int j=0;j<rowSize;j++){
-            cin>>arr[i][j];
-        }
-    }
-}
-
-outputMatrix(int arr[], int rowSize){
-    for(int i=0;i<rowSize;i++){
-        for(int j=0;j<rowSize;j++){
-            cout<<arr[i][j];
-        }
-        cout<<endl;
-    }
-}
-
 int main(){
-    int arr1[2][2],arr2[2][2];
-    inputMatrix(arr1,2,2);
-    inputMatrix(arr2,2,2);
-    multiMatrix(arr1,arr2);
+    pair<int,int>  g1;         //default
+    g1.first=5;
+    g1.second=7;
+    cout<<g1.first<<" "<<g1.second<<endl;
+
+
+
+    pair  g2(1, 'a');  //initialized,  different data type
+    cout<<g2.first<<" "<<g2.second<<endl;
+
+    pair  g3(1, 10);   //initialized,  same data type
+    cout<<g3.first<<" "<<g3.second<<endl;
+    
+    pair  g4(g3);    //copy of g3
+    cout<<g4.first<<" "<<g4.second<<endl;
+    
     return 0;
 }
