51830a9b35e0616ab918003dce1b6804acdd1dad
ma-anand <aanand20-comp@bvucoep.edu.in>
2023-03-30 00:50:05 +0530

    Square root (decmil place also)

--- v9 Binary Search/v9_5.cpp
+++ v9 Binary Search/v9_5.cpp
@@ -3,9 +3,40 @@
 #include<iostream>
 using namespace std;
 
+int squareRootInInt(int num){
+    int start=0,end=num,ans=0;
+    int mid=start+(end-start)/2;
+    while(start<=end){
+        if(mid*mid==num){
+            return mid;
+        }
+        else if(mid*mid>num){
+            end=mid-1;
+        }
+        else{
+            ans=mid;
+            start=mid+1;
+        }
+        mid=start+(end-start)/2;
+    }
+    return ans;
+}
+
+double precision(int num, int place, int tempSol){
+    double fact=1, ans=tempSol;
+    for(int i=0; i<place; i++){
+        fact/=10;
+        for(double j=ans;j*j<num; j+=fact){
+            ans=j;
+        }
+    }
+    return ans;
+}
+
 int main(){
     int num,dec;
     cin>>num>>dec;
-    cobinarySearch(num);
+    int tempSol=squareRootInInt(num);
+    cout<<"Squre root of "<<num<<" is "<<precision(num,dec,tempSol)<<endl;
     return 0;
 }
