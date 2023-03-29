3804cf792d93c37459a06e012f2267a1e69cd7ad
ma-anand <aanand20-comp@bvucoep.edu.in>
2023-03-29 22:37:43 +0530

    Square root (decmil place also)

--- v9 Binary Search/v9_5.cpp
+++ v9 Binary Search/v9_5.cpp
@@ -1,9 +1,11 @@
-//
+//Square root (decmil place also)
 //28th of March,2023
 #include<iostream>
 using namespace std;
 
 int main(){
-    
+    int num,dec;
+    cin>>num>>dec;
+    cobinarySearch(num);
     return 0;
 }
