66ed0c6b7ab40803376f6c8fd4f6ea39cac783f5
ma-anand <aanand20-comp@bvucoep.edu.in>
2023-04-05 23:37:01 +0530

    Sort

--- v9 Binary Search/v9_6.cpp
+++ v9 Binary Search/v9_6.cpp
@@ -1,7 +1,16 @@
 //
 //30th of March, 2023
+#include <iostream>
+#include <algorithm>
+using namespace std;
 
-int mian(){
-    int a;
-    return 0;
+int main()
+{
+	int a[10] = {9,5,4,3,2,1,6,7,10,8}; 
+	sort(a, a+10);
+	 				
+	for(int i = 0; i <10; ++i)
+	{
+		cout << a[i] << " ";
+	}
 }
